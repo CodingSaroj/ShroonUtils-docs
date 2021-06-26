b1e85a7b6d1a74f7def463b34f2541b1f67b5522
Saroj Kumar <sarojkr699681@gmail.com>
2021-06-26 10:41:55 +0530

    Updated documentation.

--- api/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['shrn_5fstructure_5fdefault_5ferror_5freporter_85',['SHRN_STRUCTURE_DEFAULT_ERROR_REPORTER',['../ErrorReporter_8hpp.html#aa597ef9c4c9bf360e52aa1d5aff96322',1,'ErrorReporter.hpp']]]
-];
